9e458bbb4140c0a40f1fefaca50f0f18fe4fe27e
Danilo Alves <danilo.alves@gmail.com>
2016-07-10 03:24:45 -0300

    Start

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: daniloalves
+ *
+ * Created on 10 de Julho de 2016, 03:22
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    /*HORA DO SHOW*/
+    return 0;
+}
+
